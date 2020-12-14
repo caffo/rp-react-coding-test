c430c3cc04fcbf836187ab1cba53110ada716f5b
renanigt <renanigt@gmail.com>
2020-12-14 02:13:49 -0300

    load table from api

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import Table from './components/Table';
 
 import './App.css';
 
+const table_api = 'https://storage.googleapis.com/replaypoker-dummy-api/tables/8.json'
+
 class App extends Component {
-  static propTypes = {
-    table: tableShape.isRequired,
+  constructor(props) {
+    super(props)
+    this.state = { table: {}, isLoading: true }
+  }
+
+  componentDidMount() {
+    fetch(table_api).then(response =>
+      response.json()
+    ).then(result =>
+      this.setState({table: result, isLoading: false})
+    )
   }
 
   render() {
-    const { table } = this.props;
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
 
     return (
       <div className="App">
-        <Table table={table} />
+        <Table table={this.state.table} />
       </div>
     );
   }
