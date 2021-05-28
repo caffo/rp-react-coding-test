f1914d43081828d786efce92799c952c0ac80333
Eduardo Omine <eduardo.omine@gmail.com>
2021-05-28 12:28:56 -0300

    Makes unit test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import App from './App';
 
-import table from './data/table-1'
-
 it('renders without crashing', () => {
+  const mockStore = configureStore([thunk]);
+  const store = mockStore({
+    table: {
+      isLoading: false,
+      error: null,
+      table: null,
+    },
+  });
   const div = document.createElement('div');
-  ReactDOM.render(<App table={table} />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
