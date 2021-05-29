56406ad092ddc2cfc67807ee6f9f9b5178ec0c86
Eduardo Omine <eduardo.omine@gmail.com>
2021-05-29 11:08:47 -0300

    Adds checks for undefined table.currentHand (table id = 5)

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -10,9 +10,9 @@ import './Table.css';
 const Table = ({ table }) => (
   <div className="Table">
     <div>
-      <Seats seats={table.seats} players={table.currentHand.players} />
-      <Cards values={table.currentHand.communityCards} />
-      <Pots pots={table.currentHand.pots} />
+      <Seats seats={table.seats} players={table.currentHand && table.currentHand.players} />
+      { table.currentHand && <Cards values={table.currentHand.communityCards} /> }
+      { table.currentHand && <Pots pots={table.currentHand.pots} /> }
     </div>
   </div>
 );
